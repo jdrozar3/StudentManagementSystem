eb76d1814ae480aa1b6616de9adab2afcb8046ec
jdrozar3 <rozariomichael473@gmail.com>
2021-01-28 16:41:06 -0500

    updated all files

---
+++ src/tests/Tests.java
@@ -0,0 +1,37 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import sysImplementation.Course;
+import sysImplementation.Student;
+
+public class Tests {
+
+	@Test
+	public void test() {
+		Student student = new Student(116911894, "Michael" , "Computer Science");
+		student.registerForCourse("CMSC216", 4, "Spring", "2021");
+		student.registerForCourse("CMSC132", 4, "Spring", "2021");
+		student.setGrades("Spring", "2021", "CMSC216", 'A');
+		student.setGrades("Spring", "2021", "CMSC132", 'B');
+		student.completeCourses("Spring", "2021");
+		
+		
+
+		System.out.println(student.getName());
+		//System.out.println(student.getCoursesRegistered());
+	 ArrayList<Course> list =	student.getCoursesCompleted().get("Spring").get("2021");
+	 for(int i = 0; i < list.size(); i++) {
+		 Course item = list.get(i);
+		 System.out.println(item.toString());
+	 }
+
+
+		
+	}
+
+}
